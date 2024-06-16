ea2bf47b449897ec85558956d2c8266b9cb03415
Farhad Zada <farhad.szd@gmail.com>
2024-06-16 11:32:46 +0400

    Workflows

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://farhad-zada.github.io/soros-frontie/tonconnect-manifest.json`';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
